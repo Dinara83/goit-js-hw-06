870182af3a43718eb7b4e8ea7f3b9342d88a637c
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-04 12:22:46 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const elements = [];
+
+const listEl = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+
+  return elements.push(itemEl);
+});
+listEl.append(...elements);
