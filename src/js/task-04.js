2056a585f081a5f74fd8c37b0882686a3c0d6689
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-05 23:34:57 +0200

    task-4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,3 +1,25 @@
-const counterEl = document.querySelector('#counter');
-console.log(counterEl);
-const counterValue = 0;
+const refs = {
+  addBtn: document.querySelector('[data-action="decrement"]'),
+  value: document.querySelector('#value'),
+  subBtn: document.querySelector('[data-action="increment"]'),
+};
+console.log(refs);
+
+let counterValue = 0;
+
+const render = () => {
+  refs.value.textContent = counterValue;
+};
+
+const handleDecrement = () => {
+  counterValue -= 1;
+  render();
+};
+const handleIncrement = () => {
+  counterValue += 1;
+  render();
+};
+
+render();
+refs.addBtn.addEventListener('click', handleDecrement);
+refs.subBtn.addEventListener('click', handleIncrement);
