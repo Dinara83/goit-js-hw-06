7e99543ff92952a55b3760f80d212c38a70723f9
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-06 17:17:38 +0200

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,14 @@
+const formRef = document.querySelector('.login-form');
 
+function onSubmit(e) {
+  e.preventDefault();
+  const { email, password } = e.currentTarget.elements;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  if (!emailValue || !passwordValue) {
+    return alert('Всі поля повинні бути заповнені');
+  }
+  console.log({ email: emailValue, passwordValue: passwordValue });
+  e.currentTarget.resert();
+}
+formRef.addEventListener('submit', onSubmit);
