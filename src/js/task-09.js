2ba5f970bcc030c395e842c2594c38f53ab9eab7
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-06 17:51:54 +0200

    task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const widgetRef = document.querySelector('.widget');
+const spanRef = document.querySelector('.color');
+const btnRef = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnRef.addEventListener('click', onClick);
+
+function onClick(event) {
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  spanRef.textContent = color;
 }
